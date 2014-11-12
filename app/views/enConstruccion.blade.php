f219b94cce0c26100ce3451598121fb83cdb9bf3
valentinsvt <valentinsvt@hotmail.com>
2014-11-12 18:13:45 -0500

    archivos

--- app/views/enConstruccion.blade.php
+++ app/views/enConstruccion.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="alert alert-warning" style="margin: 50px;">
         <i class="fa fa-warning fa-2x" style="margin-right: 10px;"></i>
-        Sitio en construcción!!!
+        Sitio en construcción
         <img src="{{ URL::asset('assets/images/working.GIF') }}" alt="En construcción"/>
     </div>
 @stop
