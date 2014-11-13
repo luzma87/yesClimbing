4fe6dd5ae18940fc18c6de0eadc7bff4017d4f43
valentinsvt <valentinsvt@hotmail.com>
2014-11-13 01:17:37 -0500

    diseño casi todo

--- app/views/enConstruccion.blade.php
+++ app/views/enConstruccion.blade.php
@@ -3,10 +3,28 @@
 @title('En construcción')
 
 @section('content')
-<div class="top"></div>
-    <div class="" style="margin: 50px;border:1px solid black">
-        <i class="fa fa-warning fa-2x" style="margin-right: 10px;"></i>
-        Sitio en construcción 
-        <img src="{{ URL::asset('assets/images/working.GIF') }}" alt="En construcción"/>
+<div class="top col-xs-12 col-md-12 col-lg-12"></div>
+<div class="banner col-xs-12 col-md-12 col-lg-12">
+    <img  class="imgBanner" src="{{ URL::asset('assets/images/montana2.jpg') }}" style="margin: 0px"/>
+    <div class="row upper-row">
+
+        <div class="logo col-xs-3 col-md-3 col-sm-3 col-lg-2 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
+            <img class="imgBanner" src="{{ URL::asset('assets/images/logo.PNG') }}" style="width: 100%;margin: 0px"/>
+        </div>
+
+        <div class="col-lg-7 col-md-6 col-sm-7 col-xs-3 menu-horizontal col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-2" >
+            <a href="#" class="btn-menu-horizontal col-lg-2 col-md-2 col-sm-2  col-xs-12 ui-corner-all active">INICIO</a>
+            <a href="#" class="btn-menu-horizontal col-lg-2 col-md-2 col-sm-2 col-xs-12 ui-corner-all">NOSOTROS</a>
+            <a href="#" class="btn-menu-horizontal col-lg-2 col-md-2 col-sm-2 col-xs-12 ui-corner-all">COMENTARIOS</a>
+            <a href="#" class="btn-menu-horizontal col-lg-2 col-md-2 col-sm-2 col-xs-12 ui-corner-all">CONTACTO</a>
+        </div>
     </div>
+
+
+</div>
+<div class="row bottom-row" >
+    <div class="col-lg-4 col-lg-offset-1">
+        Cultural and adventure!!
+    </div>
+</div>
 @stop
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,49 +1,51 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
 
-        {{ HTML::style('assets/bootstrap-3.3.0/css/bootstrap.min.css') }}
-        {{ HTML::style('assets/bootstrap-3.3.0/css/bootstrap-theme-cerulean.min.css') }}
+    {{ HTML::style('assets/bootstrap-3.3.0/css/bootstrap.min.css') }}
+    {{ HTML::style('assets/bootstrap-3.3.0/css/bootstrap-theme-cerulean.min.css') }}
 
-        {{ HTML::style('assets/font-awesome-4.2.0/css/font-awesome.min.css') }}
+    {{ HTML::style('assets/font-awesome-4.2.0/css/font-awesome.min.css') }}
 
-        {{ HTML::style('assets/js/jquery-ui-1.11.2.custom/jquery-ui.min.css') }}
-        {{ HTML::style('assets/js/jquery-ui-1.11.2.custom/jquery-ui.structure.min.css') }}
-        {{ HTML::style('assets/js/jquery-ui-1.11.2.custom/jquery-ui.theme.min.css') }}
-        {{ HTML::style('assets/css/estilos.css') }}
+    {{ HTML::style('assets/js/jquery-ui-1.11.2.custom/jquery-ui.min.css') }}
+    {{ HTML::style('assets/js/jquery-ui-1.11.2.custom/jquery-ui.structure.min.css') }}
+    {{ HTML::style('assets/js/jquery-ui-1.11.2.custom/jquery-ui.theme.min.css') }}
+    {{ HTML::style('assets/css/estilos.css') }}
 
-        <link rel="apple-touch-icon" sizes="57x57" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-57x57.png') }}">
-        <link rel="apple-touch-icon" sizes="114x114" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-114x114.png') }}">
-        <link rel="apple-touch-icon" sizes="72x72" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-72x72.png') }}">
-        <link rel="apple-touch-icon" sizes="144x144" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-144x144.png') }}">
-        <link rel="apple-touch-icon" sizes="60x60" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-60x60.png') }}">
-        <link rel="apple-touch-icon" sizes="120x120" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-120x120.png') }}">
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-76x76.png') }}">
-        <link rel="apple-touch-icon" sizes="152x152" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-152x152.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-180x180.png') }}">
-        <meta name="apple-mobile-web-app-title" content="Yes Climbing Guides">
-        <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-192x192.png" sizes="192x192') }}">
-        <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-160x160.png" sizes="160x160') }}">
-        <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-96x96.png" sizes="96x96') }}">
-        <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-16x16.png" sizes="16x16') }}">
-        <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-32x32.png" sizes="32x32') }}">
-        <meta name="msapplication-TileColor" content="#2b5797">
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png">
-        <meta name="application-name" content="Yes Climbing Guides">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('assets/images/favicons/apple-touch-icon-180x180.png') }}">
+    <meta name="apple-mobile-web-app-title" content="Yes Climbing Guides">
+    <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-192x192.png" sizes="192x192') }}">
+    <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-160x160.png" sizes="160x160') }}">
+    <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-96x96.png" sizes="96x96') }}">
+    <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-16x16.png" sizes="16x16') }}">
+    <link rel="icon" type="image/png" href="{{ URL::asset('assets/images/favicons/favicon-32x32.png" sizes="32x32') }}">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="application-name" content="Yes Climbing Guides">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    @yield('header')
+</head>
+<body>
+<div class="container">
+    @yield('content')
 
-        <title>@yield('title')</title>
-        @yield('header')
-    </head>
-    <body>
-       @yield('content')
+    @yield('footer')
 
-       @yield('footer')
+    {{ HTML::script('assets/js/jquery-ui-1.11.2.custom/jquery-1.11.1.min.js') }}
+    {{ HTML::script('assets/js/jquery-ui-1.11.2.custom/jquery-ui.min.js') }}
+    {{ HTML::script('assets/bootstrap-3.3.0/js/bootstrap.min.js') }}
 
-        {{ HTML::script('assets/js/jquery-ui-1.11.2.custom/jquery-1.11.1.min.js') }}
-        {{ HTML::script('assets/js/jquery-ui-1.11.2.custom/jquery-ui.min.js') }}
-        {{ HTML::script('assets/bootstrap-3.3.0/js/bootstrap.min.js') }}
-
-       @yield('scripts')
-    </body>
+    @yield('scripts')
+</div>
+</body>
 </html>
